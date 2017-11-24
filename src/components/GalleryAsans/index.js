41e72e96a702b8d142339994373f31cbae8dcea3
KarovInal <bagov.ali07@gmail.com>
2017-11-24 16:34:41 +0300

    init gallery asan folder

---
+++ src/components/GalleryAsans/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class GalleryAsans extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Asana
+            </div>
+        );
+    }
+}
+
+export default GalleryAsans;
